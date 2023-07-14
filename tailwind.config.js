344a557a3ffdf5a167a1e2471528e142b35add68
jjanmo <jjanmo07@hanmail.net>
2023-07-15 01:59:33 +0900

    feat: add logo animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,38 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'move-right': {
+          '100%': {
+            opacity: 1,
+            transform: 'translateX(64px)',
+          },
+        },
+        'move-right-more': {
+          '100%': {
+            opacity: 1,
+            transform: 'translateX(128px)',
+          },
+        },
+        'spin-scale': {
+          '0%': {
+            transform: 'rotate(0deg) scale(1)',
+          },
+          '50%': {
+            transform: 'rotate(180deg) scale(1.25)',
+          },
+          '100%': {
+            transform: 'rotate(360deg) scale(1)',
+          },
+        },
+      },
+      animation: {
+        'move-right': 'move-right 1s .5s ease-out forwards',
+        'move-right-more': 'move-right-more 1.5s .8s ease-out forwards',
+        'spin-scale': 'spin-scale 3s 1.2s linear infinite',
+      },
+    },
   },
   plugins: [],
   darkMode: 'class',
