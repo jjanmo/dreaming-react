2c7c6d91e4c2379b38a35c8ca94e75509f5b6cb4
jjanmo <jjanmo07@hanmail.net>
2024-10-19 19:50:00 +0900

    fix: lazy import 로 환경분리

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,10 +1,19 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import Nav from '@components/common/Nav'
+import { lazy, Suspense } from 'react'
 
 export const Route = createRootRoute({
   component: () => <Root />,
 })
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null
+    : lazy(() =>
+        import('@tanstack/router-devtools').then((res) => ({
+          default: res.TanStackRouterDevtools,
+        }))
+      )
 
 const Root = () => {
   return (
@@ -14,7 +23,10 @@ const Root = () => {
       <main>
         <Outlet />
       </main>
-      <TanStackRouterDevtools />
+
+      <Suspense>
+        <TanStackRouterDevtools />
+      </Suspense>
     </>
   )
 }
