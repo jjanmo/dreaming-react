f63831ab81363f012dc3c98a9477ae70e7aeceed
jjanmo <jjanmo07@hanmail.net>
2024-11-15 00:49:19 +0900

    fix: 포커싱에 따른 스타일 변경 기능 추가

--- src/components/yuotube/SearchForm.tsx
+++ src/components/yuotube/SearchForm.tsx
@@ -1,12 +1,27 @@
+import { useState } from 'react'
 import { CiSearch } from 'react-icons/ci'
 
 const SearchForm = () => {
+  const [isFocus, setIsFocus] = useState<boolean>(true)
+
+  const handleFocus = () => {
+    setIsFocus(true)
+  }
+
+  const handleBlur = () => {
+    setIsFocus(false)
+  }
+
   return (
-    <form className="w-[640px] h-10 mx-auto rounded-3xl flex border-[2px] border-slate-200 border-solid overflow-hidden">
-      <input type="text" className="flex-auto pl-4 outline-none text-black" placeholder="검색" />
+    <form className={`w-[640px] h-10 mx-auto flex`} onFocus={handleFocus} onBlur={handleBlur}>
+      <input
+        type="text"
+        className={`flex-auto pl-4 text-black border-2 border-${isFocus ? '[#1C62B9]' : 'slate'}-200 border-solid rounded-l-full`}
+        placeholder="검색"
+      />
       <button
         type="submit"
-        className="w-16 h-auto flex justify-center items-center bg-slate-50 dark:bg-gray-500"
+        className="w-16 h-auto flex justify-center items-center bg-slate-50 dark:bg-gray-500 rounded-r-full border-slate-200 border-solid border-t-2 border-r-2 border-b-2"
       >
         <CiSearch size={24} className="text-black dark:text-white" />
       </button>
