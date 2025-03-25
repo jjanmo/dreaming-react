f98dff25ad99faba237b940969c9cd133765197d
jjanmo <jjanmo07@hanmail.net>
2025-03-26 00:42:10 +0900

    feat: api layer 구현 + youtube api 구현

---
+++ src/apis/youtube.ts
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+const youtubeApi = axios.create({
+  baseURL: import.meta.env.VITE_YOUTUBE_API_URL,
+  params: {
+    key: import.meta.env.VITE_YOUTUBE_API_KEY,
+  },
+})
+
+export const getPopularVideos = (regionCode = 'KR', maxResults = 25) => {
+  return youtubeApi.get('/videos', {
+    params: {
+      part: 'snippet',
+      chart: 'mostPopular',
+      maxResults,
+      regionCode,
+    },
+  })
+}
+
+export const searchVideos = (query: string, regionCode = 'KR', maxResults = 25) => {
+  return youtubeApi.get('/search', {
+    params: {
+      part: 'snippet',
+      maxResults,
+      regionCode,
+      q: query,
+    },
+  })
+}
