fbe78f0eb8bae5938c8d1e536517e723ffd888dc
jjanmo <jjanmo07@hanmail.net>
2025-03-26 01:52:33 +0900

    feat: 비디오 목록 구현-파트1

--- src/pages/Youtube.tsx
+++ src/pages/Youtube.tsx
@@ -1,9 +1,11 @@
-import SearchForm from '@components/yuotube/SearchForm'
+import SearchForm from '@components/youtube/SearchForm'
+import VideoList from '@components/youtube/VideoList'
 
 const Youtube = () => {
   return (
     <section className="pt-5">
       <SearchForm />
+      <VideoList />
     </section>
   )
 }
